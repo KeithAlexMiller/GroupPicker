38f563af3d616e92acd8ba9db28c098bb78001a2
Keith Miller <keithalexmiller@gmail.com>
2015-04-19 16:00:10 -0600

    RandomGroupPicker

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -9,52 +9,45 @@ namespace ConsoleApplication3
     class Program
     {
         
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             List<string> studentList = new List<string>() { "David", "Umar", "Matt", "Aaron", "Mahmoud", "Mitch", "Kris", "Ryan", "Coulton", "Mac", "Lamond", "Nathan", "Keith" };
             PickGroup(studentList, 4);
             Console.ReadKey();
         }
-        public static void PickGroup(List<string> groupList, int groupSize)
+        public static void PickGroup(List<string> studentList, int groupSize)
         {
+            //current list of randomly chosen students
             List<string> currentGroupList = new List<string>();
+
+            //used to denote group number and increments when new group is created
             int groupNumber = 1;
-            //int count = studentList.Count();
-            Random rnd = new Random();
-            string currentStudent = string.Empty;
-            while (groupList.Count > 0)
+
+            //random number generator
+            Random rng = new Random();
+
+            //randomly choose a student name from studentList and repeat
+            for (int i = studentList.Count - 1; i >= 0; i--)
             {
-                int index = rnd.Next(groupList.Count);
-                currentStudent = groupList[index];
-                if (groupList.Count > groupSize)
+                //initiate random selection of name and add to current list and remove from original
+                string currentStudent = studentList[rng.Next(0, i + 1)];
+                currentGroupList.Add(currentStudent);
+                studentList.Remove(currentStudent);
+
+                //when currentGroupList has groupSize names
+                if ((currentGroupList.Count == groupSize) || (studentList.Count == 0))
                 {
-                    // get groupSize# of currentStudent and add to currentGroupList
-                    currentGroupList.Add(currentStudent); // do (groupSize) times;
-                    groupList.RemoveAt(index);
-                }
-                else if (groupList.Count <= groupSize && groupList.Count > 0)
-                {
-                    currentGroupList.Add(currentStudent); 
-                    groupList.RemoveAt(index);
-                }
-                if (currentGroupList.Count == groupSize)
-                {
-                    Console.WriteLine("Group: " + groupNumber);
-                    Console.WriteLine(currentGroupList[0]);
-                    Console.WriteLine(currentGroupList[1]);
-                    Console.WriteLine(currentGroupList[2]);
-                    Console.WriteLine(currentGroupList[3]);
+                    Console.WriteLine("GROUP {0}", groupNumber);
+                    for (int j = 0; j < currentGroupList.Count; j++)
+                    {
+                        Console.WriteLine("{0}", currentGroupList[j]);
+                    }
+                    Console.WriteLine();
+                    //remove names from currentGroupList
                     currentGroupList.Clear();
                     groupNumber++;
                 }
-                else if (currentGroupList.Count < groupSize)
-                {
-                    Console.WriteLine("Group: " + groupNumber);
-                    Console.WriteLine(currentGroupList[0]);
-                    currentGroupList.Clear();
-                }
-
             }
-        }
+        }        
     }
 }
